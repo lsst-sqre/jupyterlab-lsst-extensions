10dd03514ef57c94767772903e5551215dda7922
adam <athornton@gmail.com>
2019-08-19 14:39:07 -0700

    Add API query template too.

--- jupyterlab_lsstextensions/templatemap.py
+++ jupyterlab_lsstextensions/templatemap.py
@@ -3,7 +3,7 @@ This defines a git repository for each known query_type.
 """
 
 TEMPLATEMAP = {
-    "api": {"url": "https://something",
+    "api": {"url": "https://github.com/lsst-sqre/lsst-apiquerytemplate",
             "branch": "master",
             "subdir": None},
     "squash": {"url": "https://github.com/lsst-sqre/squash-bokeh",
